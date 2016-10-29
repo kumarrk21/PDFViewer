ccf22e2bf244aed84968fbdc93edbfbf2fac21c5
KK Ramamoorthy <kumarrk21@gmail.com>
2016-10-29 11:33:11 -0400

    Updates to example component
    Signed-off-by: KK Ramamoorthy <kumarrk21@gmail.com>

--- src/aura/pdfViewer/pdfViewerHelper.js
+++ src/aura/pdfViewer/pdfViewerHelper.js
@@ -4,8 +4,9 @@
 		try{
 			var pdfData = component.get('v.pdfData');
 			var pdfjsframe = component.find('pdfFrame')
-			var cloned = JSON.parse(JSON.stringify(pdfjsframe));
-			pdfjsframe.getElement().contentWindow.postMessage(pdfData,'*');
+			if(typeof pdfData != 'undefined'){
+				pdfjsframe.getElement().contentWindow.postMessage(pdfData,'*');	
+			}
 		}catch(e){
 			alert('Error: ' + e.message);
 		}
